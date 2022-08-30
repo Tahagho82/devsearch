d10cf766d60bdb11160c9a02531fa6b49f30f749
Tahagho82 <m.tahagho@gmail.com>
2022-08-30 18:10:16 +0430

    add_a_template

--- projects/forms.py
+++ projects/forms.py
@@ -1,11 +1,20 @@
-from dataclasses import field
-import imp
-from django.forms import ModelForm
-from .models import Project
+from django.db.models.base import Model
+from django.forms import ModelForm, widgets
+from django import forms
+from .models import Project, Review
 
 
 class ProjectForm(ModelForm):
-    class Meta():
+    class Meta:
         model = Project
         fields = ['title', 'featured_image', 'description',
-                  'demo_link', 'source_link', 'tags']
+                  'demo_link', 'source_link']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
